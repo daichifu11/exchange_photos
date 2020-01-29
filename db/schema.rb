2f5cc45bc80ee62c723a59d1ccd14a982fa3544c
Daichi-Fukuta <ddd.ddd777ff@gmail.com>
2020-01-30 08:37:24 +0900

    git操作ミスの訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_233002) do
-
+ActiveRecord::Schema.define(version: 2020_01_23_023309) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
