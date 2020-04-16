d31bbef4931307f168514901f1cb3d2470e90bff
Daichi-Fukuta <ddd.ddd777ff@gmail.com>
2020-04-16 23:05:51 +0900

    パスワードがなしでプロフィール画像編集できないの修正したけど動かなかったから一旦戻した

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,14 +51,4 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
-  def update_without_current_password(params, *options)
-    params.delete(:current_password)
-    if params[:password].blank? && params[:password_confirmation].blank?
-      params.delete(:password)
-      params.delete(:password_confirmation)
-    end
-    result = update_attributes(params, *options)
-    clean_up_passwords
-    result
-  end
 end
